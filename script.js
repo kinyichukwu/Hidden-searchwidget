401f7b3a5234eeb77d28d640afcd8c5a988adf1f
kinyichukwu <81447346+kinyichukwu@users.noreply.github.com>
2022-02-27 19:15:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var search = document.querySelector('.search')
+var click = document.getElementById('btn')
+var input = document.querySelector('.input')
+
+click.addEventListener('click', ()=>{
+    search.classList.toggle('active')
+})
+
